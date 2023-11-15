c8f22d900099709c0a0d3b3717ab2d6f9313c4c8
andres <acarrillo@studenti.apuliadigitalmaker.it>
2023-11-15 17:23:51 +0100

    commit backend

---
+++ backend/PrenotazioniPostazioni/src/main/java/it/linksmt/prenotazoni/PrenotazioniPostazioni/PrenotazioniPostazioniApplication.java
@@ -0,0 +1,13 @@
+package it.linksmt.prenotazoni.PrenotazioniPostazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenotazioniPostazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrenotazioniPostazioniApplication.class, args);
+	}
+
+}
---
+++ backend/PrenotazioniPostazioni/src/test/java/it/linksmt/prenotazoni/PrenotazioniPostazioni/PrenotazioniPostazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package it.linksmt.prenotazoni.PrenotazioniPostazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrenotazioniPostazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
