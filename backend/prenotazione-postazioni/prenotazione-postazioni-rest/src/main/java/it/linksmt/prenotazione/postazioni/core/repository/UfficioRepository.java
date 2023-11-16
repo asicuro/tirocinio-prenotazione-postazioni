a8f3055630d0b38cefe0e7022b30f1f6f0d4d4d3
Daniel.Zotti17 <d.zotti@studenti.apuliadigitalmaker.it>
2023-11-16 15:17:54 +0100

    Ufficio Repo aggiustato

--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/repository/UfficioRepository.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/repository/UfficioRepository.java
@@ -1,7 +1,12 @@
 package it.linksmt.prenotazione.postazioni.core.repository;
 
-import org.springframework.stereotype.Repository;
-@Repository
-public interface UfficioRepository {
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.CrudRepository;
+
+import it.linksmt.prenotazione.postazioni.core.model.Ufficio;
+
+@EnableJpaRepositories
+public interface UfficioRepository extends CrudRepository<Ufficio, Long>, JpaSpecificationExecutor<Ufficio>{
 
 }
