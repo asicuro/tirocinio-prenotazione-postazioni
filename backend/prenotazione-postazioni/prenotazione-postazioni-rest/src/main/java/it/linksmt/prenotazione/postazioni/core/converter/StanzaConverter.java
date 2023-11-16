60ec3886064268838bbfbea5de6dfd0ca190ef2b
JoeRJoe99 <gminerba@studenti.apuliadigitalmaker.com>
2023-11-16 17:30:08 +0100

    Stanza Converter

--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/converter/StanzaConverter.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/converter/StanzaConverter.java
@@ -1,6 +1,7 @@
 package it.linksmt.prenotazione.postazioni.core.converter;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import it.linksmt.prenotazione.postazioni.core.dto.StanzaDto;
 import it.linksmt.prenotazione.postazioni.core.model.Stanza;
@@ -8,5 +9,6 @@ import it.linksmt.prenotazione.postazioni.core.model.Stanza;
 @Mapper(componentModel = "spring" , uses = {PostazioneConverter.class})
 public interface StanzaConverter {
 
+	@Mapping(target = "ufficio_id", source = "Ufficio.id")
 	StanzaDto toDto(Stanza stanza);
 }
