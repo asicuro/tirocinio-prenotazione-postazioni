709c27fe59a0e7fdaf09ab6ed1421770e84a4f90
Daniel.Zotti17 <d.zotti@studenti.apuliadigitalmaker.it>
2023-11-16 17:08:39 +0100

    Ufficio Converter aggiunto

--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/converter/UfficioConverter.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/converter/UfficioConverter.java
@@ -1,5 +1,12 @@
 package it.linksmt.prenotazione.postazioni.core.converter;
 
+import org.mapstruct.Mapper;
+
+import it.linksmt.prenotazione.postazioni.core.dto.UfficioDto;
+import it.linksmt.prenotazione.postazioni.core.model.Ufficio;
+
+@Mapper(componentModel = "spring", uses = {StanzaConverter.class})
 public interface UfficioConverter {
 
+	UfficioDto toDto(Ufficio ufficio);
 }
