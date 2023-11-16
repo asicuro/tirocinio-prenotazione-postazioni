8cfa6e1aa5074a1be0fa4b68f76f6db7f8d8fa95
Daniel.Zotti17 <d.zotti@studenti.apuliadigitalmaker.it>
2023-11-16 12:58:48 +0100

    Create relazione Ufficio-Stanza + Get and Set

--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/model/Ufficio.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/model/Ufficio.java
@@ -6,7 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import java.util.List;
 
 @Entity
 @Table(name ="ufficio")
@@ -24,7 +27,61 @@ public class Ufficio {
     @Column(name = "edit_date")
     private Date editDate;
     @Column(name = "indirizzo")
-    private String Indirizzo;
+    private String indirizzo;
     @Column(name = "nome_ufficio")
     private String nomeUfficio;
+    @OneToMany(mappedBy = "ufficio")
+    private List<Stanza> stanze;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getCreateUserId() {
+		return createUserId;
+	}
+	public void setCreateUserId(Long createUserId) {
+		this.createUserId = createUserId;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public Long getEditUserId() {
+		return editUserId;
+	}
+	public void setEditUserId(Long editUserId) {
+		this.editUserId = editUserId;
+	}
+	public Date getEditDate() {
+		return editDate;
+	}
+	public void setEditDate(Date editDate) {
+		this.editDate = editDate;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	public String getNomeUfficio() {
+		return nomeUfficio;
+	}
+	public void setNomeUfficio(String nomeUfficio) {
+		this.nomeUfficio = nomeUfficio;
+	}
+	public List<Stanza> getStanze() {
+		return stanze;
+	}
+	public void setStanze(List<Stanza> stanze) {
+		this.stanze = stanze;
+	}
+    
+    
 }
+
+
