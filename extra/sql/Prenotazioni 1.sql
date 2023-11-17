14553da6a3e3fac5dc4889bb88324354ef9dba22
Giorgio Minerba <giorgio.minerba@linksmt.it>
2023-11-17 17:45:31 +0100

    SQL updated

--- extra/sql/Prenotazioni 1.sql
+++ extra/sql/Prenotazioni 1.sql
@@ -1,9 +1,7 @@
--- Active: 1699978647858@@127.0.0.1@5432@prenotazioni
-/* 
 Create  Table utente(
     id BIGINT NOT NULL,
     username VARCHAR(255),
-    psw VARCHAR(255),
+    pwd VARCHAR(255),
     ruolo VARCHAR(255),
     Primary key(id)
 );
@@ -25,14 +23,13 @@ CREATE TABLE stanza(
     create_date TIMESTAMP,
     edit_user_id BIGINT,
     edit_date TIMESTAMP,
-    nome VARCHAR(255),
+    name VARCHAR(255),
     width FLOAT,
     height FLOAT,
     x FLOAT,
     y FLOAT,
-    posizione VARCHAR(255),
     Primary key (id),
-    ufficio_id BIGINT NOT null,
+    ufficio_id BIGINT NOT NULL,
     FOREIGN KEY (ufficio_id) REFERENCES ufficio(id)
 );
 
@@ -47,7 +44,6 @@ CREATE TABLE postazione(
     x FLOAT ,
     y FLOAT ,
     stanza_id BIGINT NOT NULL,
-    posizione VARCHAR(255),
     FOREIGN KEY (stanza_id) REFERENCES stanza(id),
     Primary key (id)
 );
@@ -63,72 +59,4 @@ CREATE TABLE prenotazione(
     postazione_id BIGINT,
     FOREIGN KEY (user_id) REFERENCES utente(id),
     FOREIGN KEY (postazione_id) REFERENCES postazione(id)
-
 );
-Create  Table utente(
-    id BIGINT NOT NULL,
-    username VARCHAR(255),
-    psw VARCHAR(255),
-    ruolo VARCHAR(255),
-    Primary key(id)
-);
-
-Create TABLE ufficio(
-    id BIGINT NOT NULL,
-    create_user_id BIGINT,
-    create_date TIMESTAMP,
-    edit_user_id BIGINT,
-    edit_date TIMESTAMP,
-    indirizzo VARCHAR(255),
-    nome_ufficio VARCHAR(255),
-    Primary key(id)
-);
-
-CREATE TABLE stanza(
-    id BIGINT NOT NULL,
-    create_user_id BIGINT,
-    create_date TIMESTAMP,
-    edit_user_id BIGINT,
-    edit_date TIMESTAMP,
-    nome VARCHAR(255),
-    width FLOAT,
-    height FLOAT,
-    x FLOAT,
-    y FLOAT,
-    posizione VARCHAR(255),
-    Primary key (id),
-    ufficio_id BIGINT NOT null,
-    FOREIGN KEY (ufficio_id) REFERENCES ufficio(id)
-);
-
-CREATE TABLE postazione(
-    id BIGINT NOT NULL,
-    create_user_id BIGINT,
-    create_date TIMESTAMP,
-    edit_user_id BIGINT,
-    edit_date TIMESTAMP,
-    width FLOAT,
-    height FLOAT,
-    x FLOAT ,
-    y FLOAT ,
-    stanza_id BIGINT NOT NULL,
-    posizione VARCHAR(255),
-    FOREIGN KEY (stanza_id) REFERENCES stanza(id),
-    Primary key (id)
-);
-
-CREATE TABLE prenotazione(
-    id BIGINT NOT NULL PRIMARY KEY,
-    create_user_id BIGINT,
-    create_date TIMESTAMP,
-    edit_user_id BIGINT,
-    edit_date TIMESTAMP,
-    data_creazione Date,
-    user_id BIGINT,
-    postazione_id BIGINT,
-    FOREIGN KEY (user_id) REFERENCES utente(id),
-    FOREIGN KEY (postazione_id) REFERENCES postazione(id)
-); */
-
-INSERT INTO utente(id, username, psw, ruolo)
-VALUES (0,'giorgio.minerba', '270599', 'admin');
