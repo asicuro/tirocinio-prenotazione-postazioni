26fa65c944b9f5987a6f5ab0b07ccfab28c825c9
Giorgio Minerba <giorgio.minerba@linksmt.it>
2023-11-17 11:31:43 +0100

    Service Utente

--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/service/api/UtenteService.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/service/api/UtenteService.java
@@ -1,6 +1,11 @@
 package it.linksmt.prenotazione.postazioni.core.service.api;
 
+import org.springframework.stereotype.Service;
 
+import it.linksmt.prenotazione.postazioni.core.dto.UtenteDto;
+
+@Service
 public interface UtenteService {
 	
+	public UtenteDto findUtente(Long id);
 }
--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/service/impl/UtenteServiceImpl.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/core/service/impl/UtenteServiceImpl.java
@@ -1,7 +1,32 @@
 package it.linksmt.prenotazione.postazioni.core.service.impl;
 
-public class UtenteServiceImpl{
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.linksmt.prenotazione.postazioni.core.converter.UtenteConverter;
+import it.linksmt.prenotazione.postazioni.core.dto.UtenteDto;
+import it.linksmt.prenotazione.postazioni.core.model.Utente;
+import it.linksmt.prenotazione.postazioni.core.repository.UtenteRepository;
+import it.linksmt.prenotazione.postazioni.core.service.api.UtenteService;
+
+public class UtenteServiceImpl implements UtenteService{
 	
+	@Autowired
+	UtenteRepository utenteRepository;
 	
+	@Autowired
+	UtenteConverter utenteConverter;
+
+	@Override
+	public UtenteDto findUtente(Long id) {
+		
+		Optional<Utente> utenteOptional = utenteRepository.findById(id);
+		if (utenteOptional.isEmpty()) {
+			return null;
+		}
+		return utenteConverter.toDto(utenteOptional.get());
+		
+	}
+
 }
