94b9598b1b2511dde7ea81d62cd6258461d4121f
Daniel.Zotti17 <d.zotti@studenti.apuliadigitalmaker.it>
2023-11-17 19:20:51 +0100

    Fix Application

--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/PrenotazionePostazioniApplication.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/PrenotazionePostazioniApplication.java
@@ -3,13 +3,16 @@ package it.linksmt.prenotazione.postazioni;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication( scanBasePackages= {"it.linksmt.prenotazione.postazioni"}, exclude = { 
 //		DataSourceAutoConfiguration.class,
 		SecurityAutoConfiguration.class })
 @PropertySources({ @PropertySource("classpath:application.yml")})
+@ComponentScan({"it.linksmt.prenotazione.postazioni.core.converter"})
 public class PrenotazionePostazioniApplication {
 
 	public static void main(String[] args) {
