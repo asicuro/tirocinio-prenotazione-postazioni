774ae1fdefaac589961fd71d9b5f796c0b806fd3
andres <acarrillo@studenti.apuliadigitalmaker.it>
2023-11-20 10:57:40 +0100

    Post Postazione/prenotazione(aka save)

--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/controller/PostazioneController.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/controller/PostazioneController.java
@@ -5,6 +5,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +24,12 @@ public class PostazioneController {
 	
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE )
 	public ResponseEntity<PostazioneDto> getPostazioneDto(@PathVariable(value="id")Long id) {
-		
 		return ResponseEntity.ok(postazioneService.findPostazioneById(id));		
+	}
+	
+	@PostMapping(value ="/save")
+	public void savePostazione(@RequestBody PostazioneDto postazioneDto) {
+		postazioneService.savePostazione(postazioneDto);
 	}
 	
 }
--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/controller/PrenotazioneController.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/controller/PrenotazioneController.java
@@ -4,9 +4,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import it.linksmt.prenotazione.postazioni.core.dto.PrenotazioneDto;
 import it.linksmt.prenotazione.postazioni.core.service.api.PrenotazioneService;
 import it.linksmt.prenotazione.postazioni.rest.constants.PrenotazionePostzioniConst;
@@ -21,5 +22,9 @@ public class PrenotazioneController {
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE )
 	public ResponseEntity<PrenotazioneDto> getPrenotazioneDtoEntity(@PathVariable(value="id")Long id){
 		return ResponseEntity.ok(prenotazioneService.findPrenotazioneById(id));		
+	}
+	@PostMapping(value ="/save")
+	public void savePrenotazione(@RequestBody PrenotazioneDto prenotazioneDto) {
+		prenotazioneService.savePrenotazione(prenotazioneDto);
 	}
 }
