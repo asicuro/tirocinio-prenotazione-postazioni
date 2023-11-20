5fd9c3ce38ba50c33391a948a5f80f992db7f6e8
andres <acarrillo@studenti.apuliadigitalmaker.it>
2023-11-20 10:24:44 +0100

    RestController Prenotazione

--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/controller/PrenotazioneController.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/controller/PrenotazioneController.java
@@ -2,16 +2,16 @@ package it.linksmt.prenotazione.postazioni.rest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.linksmt.prenotazione.postazioni.core.dto.PrenotazioneDto;
 import it.linksmt.prenotazione.postazioni.core.service.api.PrenotazioneService;
 import it.linksmt.prenotazione.postazioni.rest.constants.PrenotazionePostzioniConst;
 
-@Controller
+@RestController
 @RequestMapping (PrenotazionePostzioniConst.PRENOTAZIONE_PATH)
 public class PrenotazioneController {
 
