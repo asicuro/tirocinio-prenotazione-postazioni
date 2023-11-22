abf552b701f632c80d728b77366f98e5a9bcf4f4
Giorgio Minerba <giorgio.minerba@linksmt.it>
2023-11-22 15:55:36 +0100

    Error Handler

--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/handler/RestExceptionHandler.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/handler/RestExceptionHandler.java
@@ -1,5 +1,37 @@
 package it.linksmt.prenotazione.postazioni.rest.handler;
 
-public class RestExceptionHandler {
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import it.linksmt.prenotazione.postazioni.core.exceptions.InvalidValueException;
+import it.linksmt.prenotazione.postazioni.core.exceptions.MissingValueException;
+import it.linksmt.prenotazione.postazioni.rest.output.ErrorResponse;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(InvalidValueException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected ResponseEntity<ErrorResponse> invalidValue(InvalidValueException exception) {
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setTimestamp(new Date()).setStatus(500).setMessage(exception.getMessage());
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MissingValueException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	protected ResponseEntity<ErrorResponse> missingValue(MissingValueException exception) {
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setTimestamp(new Date()).setStatus(500).setMessage(exception.getMessage());
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+	}
 
 }
--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/output/ErrorResponse.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/output/ErrorResponse.java
@@ -7,39 +7,41 @@ public class ErrorResponse {
 	private String error;
 	private Date timestamp;
 	private Integer status;
-	
+
 	public String getMessage() {
-        return message;
-    }
-	
-	public void setMessage(String message) {
-        this.message = message;
-    }
-	
-    public String getError() {
-        return error;
-    }
-    
-    public void setError(String error) {
-        this.error = error;
-    }
-    
-    public Date getTimestamp() {
-        return timestamp;
-    }
-    
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
-    
-    public Integer getStatus() {
-        return status;
-    }
-    
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-    
-    
+		return message;
+	}
+
+	public ErrorResponse setMessage(String message) {
+		this.message = message;
+		return this;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public ErrorResponse setError(String error) {
+		this.error = error;
+		return this;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public ErrorResponse setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+		return this;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public ErrorResponse setStatus(Integer status) {
+		this.status = status;
+		return this;
+	}
 
 }
