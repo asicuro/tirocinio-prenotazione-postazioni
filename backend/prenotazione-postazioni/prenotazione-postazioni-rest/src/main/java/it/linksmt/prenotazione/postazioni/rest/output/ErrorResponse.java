e2eaa9ac1baeffc057906b2fcd33becf9b638879
Daniel Zotti <daniel.zotti@linksmt.it>
2023-11-22 15:44:55 +0100

    Creato ErrorResponse

--- backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/output/ErrorResponse.java
+++ backend/prenotazione-postazioni/prenotazione-postazioni-rest/src/main/java/it/linksmt/prenotazione/postazioni/rest/output/ErrorResponse.java
@@ -1,5 +1,53 @@
 package it.linksmt.prenotazione.postazioni.rest.output;
 
+import java.util.Date;
+
 public class ErrorResponse {
+	private String message;
+	private String error;
+	private Date timestamp;
+	private Integer status;
+	
+	public ErrorResponse(String message, String error, Date timestamp, Integer status) {
+		super();
+        this.message = message;
+        this.error = error;
+        this.timestamp = timestamp;
+        this.status = status;
+	}
+	
+	public String getMessage() {
+        return message;
+    }
+	
+	public void setMessage(String message) {
+        this.message = message;
+    }
+	
+    public String getError() {
+        return error;
+    }
+    
+    public void setError(String error) {
+        this.error = error;
+    }
+    
+    public Date getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+    public Integer getStatus() {
+        return status;
+    }
+    
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+    
+    
 
 }
