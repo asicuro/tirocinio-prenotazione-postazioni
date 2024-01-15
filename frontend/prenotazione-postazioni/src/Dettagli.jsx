386d29c1f32bf6bd4161cf9d5b6fcb0e36b6e0e6
andres <acarrillo@studenti.apuliadigitalmaker.it>
2024-01-15 12:33:21 +0100

    aggiunto props, sistemato colore/font

--- frontend/prenotazione-postazioni/src/Dettagli.jsx
+++ frontend/prenotazione-postazioni/src/Dettagli.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Dettagli.css';
 
-export const DettagliPreno = () => {
+export const DettagliPreno = ({ giorno, setGiorno }) => {
+
     const dettagli = {
         id: 1,
         ufficio: 'Bari',
@@ -11,29 +12,30 @@ export const DettagliPreno = () => {
     };
     const dettagli1 = {
         CreataDa: 'Giorgio Minerba',
-        DataPrenotazione: '18/01/2024',
+        DataPrenotazione: '',
         DataCreazione: '12/01/2024',
         ModificataDa: '',
         DataModificazione: ''
     };
 
+
     return (
         <div className="Dettagli-prenotazione">
-            <h2 className='card-title text-info font-weight-bold text-left my-3'>Dettagli prenotazione</h2>
+            <h2 className='card-title text-info font-weight-bold text-left my-3'><span className='Title'>Dettagli prenotazione</span></h2>
             <div className='Dettagli-columns'>
                 <div className='Dettagli-column'>
-                    <p className='Dettagli-label'><span className='text-info'>Id:</span> {dettagli.id}</p>
-                    <p className='Dettagli-label'><span className='text-info'>Ufficio:</span> {dettagli.ufficio}</p>
-                    <p className='Dettagli-label'><span className='text-info'>Stanza:</span> {dettagli.stanza}</p>
-                    <p className='Dettagli-label'><span className='text-info'>Postazione:</span> {dettagli.postazione}</p>
-                    <p className='Dettagli-label'><span className='text-info'>Utente:</span> {dettagli.utente}</p>
+                    <p className='Dettagli-label'><span className='colore'>Id:</span> {dettagli.id}</p>
+                    <p className='Dettagli-label'><span className='colore'>Ufficio:</span> {dettagli.ufficio}</p>
+                    <p className='Dettagli-label'><span className='colore'>Stanza:</span> {dettagli.stanza}</p>
+                    <p className='Dettagli-label'><span className='colore'>Postazione:</span> {dettagli.postazione}</p>
+                    <p className='Dettagli-label'><span className='colore'>Utente:</span> {dettagli.utente}</p>
                 </div>
                 <div className='Dettagli-column'>
-                    <p className='Dettagli-label'><span className='text-info'>Creato da:</span> {dettagli1.CreataDa}</p>
-                    <p className='Dettagli-label'><span className='text-info'>Data di prenotazione:</span> {dettagli1.DataPrenotazione}</p>
-                    <p className='Dettagli-label'><span className='text-info'>Data di Creazione:</span> {dettagli1.DataCreazione}</p>
-                    <p className='Dettagli-label'><span className='text-info'>Modificato da:</span> {dettagli1.ModificataDa}</p>
-                    <p className='Dettagli-label'><span className='text-info'>Data di Modifica :</span> {dettagli1.DataModificazione}</p>
+                    <p className='Dettagli-label'><span className='colore'>Creato da:</span> {dettagli1.CreataDa}</p>
+                    <p className='Dettagli-label'><span className='colore'>Data di prenotazione:</span>{giorno.toLocaleDateString()}</p>
+                    <p className='Dettagli-label'><span className='colore'>Data di Creazione:</span> {dettagli1.DataCreazione}</p>
+                    <p className='Dettagli-label'><span className='colore'>Modificato da:</span> {dettagli1.ModificataDa}</p>
+                    <p className='Dettagli-label'><span className='colore'>Data di Modifica :</span> {dettagli1.DataModificazione}</p>
                 </div>
             </div>
         </div >
